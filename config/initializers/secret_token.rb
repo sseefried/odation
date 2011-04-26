971862f3155a09e8ae03f6462a8b3daee1fcf35e
fighella <fighella@gmail.com>
2011-04-26 22:57:04 +1000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odation::Application.config.secret_token = '50d10e808a286423500c3079463a6d4c7a06f6c2e4b53a384e27dd62171dd3d87c418816b2bd4729e0285f13a03902cc9e1d518339c47eeee9a94a5ea3117d5a'
