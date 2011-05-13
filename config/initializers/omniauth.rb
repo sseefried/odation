345d79f835a5d19339d0227641a22b2ea20bd876
fighella <fighella@gmail.com>
2011-05-14 09:19:15 +1000

    commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'ksgxAgyV29LaiVW7pVWQ', 'wiQiNSU9rrtVjIfuU1u4fUSQanVMjavMCbD3slQA9E'
   provider :facebook, '109302305823116', '6f47102b5953e0c2b36f2a2f756f29ef'
-end
+end    
+
