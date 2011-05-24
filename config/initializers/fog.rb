bfac234e9300db8d6c0ec61c17d3c473dbdb352f
fighella <fighella@gmail.com>
2011-05-25 00:19:07 +1000

    s3 again

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
         :aws_secret_access_key  => 'NxUMVX3tZhOV5UUw6pVEwMYpvBybuNTtcF+PtCFj',       # required
         :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
       }
-      config.fog_directory  = 'odation_images'                     # required
+      config.fog_directory  = 'odationimg'                     # required
      config.fog_host       = 'https://s3.amazonaws.com/'            # optional, defaults to nil
       config.fog_public     = false                                   # optional, defaults to true
       config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
