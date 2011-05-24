75406d93086c150837f633bdb623fb6766fd5df3
fighella <fighella@gmail.com>
2011-05-25 00:06:13 +1000

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,4 +9,4 @@ CarrierWave.configure do |config|
      config.fog_host       = 'https://s3.amazonaws.com/'            # optional, defaults to nil
       config.fog_public     = false                                   # optional, defaults to true
       config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-    end
+    end                  sssss
