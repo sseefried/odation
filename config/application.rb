a44b92c0981dcb8e68e4aced9bcd2e3a75ab6b9f
fighella <fighella@gmail.com>
2011-05-25 00:04:35 +1000

    s3

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,8 @@ module Odation
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+  
+      
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
@@ -39,4 +40,5 @@ module Odation
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
-end
+end 
+
